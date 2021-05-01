15737982f4cd6b22f79e29c3f96b7af70ee99069
Sagar <sagar.ghimire@kotuko.it>
2021-05-01 22:05:57 +0545

    configuration of axios and react-query

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider, } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 import App from './App.jsx';
 import './index.css';
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
